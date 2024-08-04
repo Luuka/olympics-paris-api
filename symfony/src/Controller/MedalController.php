0b7760446f18575a825b26a327a1f002e37cf603
Lucas Trebouet Voisin <lucas@MacBook-Pro-de-Lucas.local>
2024-08-05 00:10:12 +0200

    Add CORS headers

--- symfony/src/Controller/MedalController.php
+++ symfony/src/Controller/MedalController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 #[Route('/medals')]
 class MedalController extends AbstractController
 {
